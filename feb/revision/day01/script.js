70bcd9820c782310fd387b7c168237c6db597e83
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-02-22 17:38:42 +0530

    Adding File ./revision

---
+++ feb/revision/day01/script.js
@@ -0,0 +1,42 @@
+// console.log(document.body)
+// document.body.style.backgroundColor="yellow"
+// document.body.childNodes[3].innerText="h"
+
+
+
+// let a = document.getElementById("a1");
+// console.log(a);
+// let b = document.getElementsByClassName("a2");
+// console.log(b);
+// let c = document.getElementsByTagName("p");
+// console.log(c);
+
+
+
+// // in querySelector and querySelectorAll
+// // we have to use . for class and # for id and direct name for tag name
+// let d = document.querySelector(".a2"); // // only gets first match found
+// console.log(d);
+// let e = document.querySelectorAll("p");
+// console.log(e);
+
+
+
+// // //Properties
+
+// // 1. tagName - gives tag name
+// let f = a.tagName;
+// console.log(f);
+
+// // 2. innerText - gives only Visual text
+// let g = document.querySelector("ul");
+// console.log(g.innerText);
+// console.log(g.innerText="change");
+
+// // 3. innerHtml - gives tags too
+// console.log(g.innerHTML);
+// console.log(g.innerHTML="<i>change</i>");
+
+// // 4. textContent - same as innerText but gives hidden text too 
+// let h = document.querySelector("h4");
+// console.log(h.textContent)
---
+++ feb/revision/day02/script.js
@@ -0,0 +1,17 @@
+let a = document.querySelector("div");
+// console.log(a);
+
+
+// // we can use any type of attribute like id, class and etc to get or set.
+// let b = a.getAttribute("id");
+// console.log(b);
+
+// a.setAttribute("id","box1");
+// b = a.getAttribute("id");
+// console.log(b);
+
+
+
+// // Applying CSS or modifing CSS
+// a.style.backgroundColor="orange"
+// a.style.color="black"
