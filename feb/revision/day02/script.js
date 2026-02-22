cab5fa82ed1b90863672104ea176593d4cebe354
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-02-22 18:46:39 +0530

    Adding File ./ravision/day02

--- feb/revision/day02/script.js
+++ feb/revision/day02/script.js
@@ -15,3 +15,26 @@ let a = document.querySelector("div");
 // // Applying CSS or modifing CSS
 // a.style.backgroundColor="orange"
 // a.style.color="black"
+
+
+// // createElement
+let c = document.createElement("button")
+// console.log(c);
+c.innerText="click me"
+
+
+// let d = document.querySelector("#box")
+// // append - at the last
+// d.append(c)
+
+// // prepend - at the start
+// d.prepend(c)
+
+// // before - adds before 
+// d.before(c)
+
+// // after - adds after
+// d.after(c)
+
+// // remove - deletes the selected tag
+// document.querySelector("p").remove();
