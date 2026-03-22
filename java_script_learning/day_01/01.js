21bc8de531944123e550d3de9f97c004d252fe7e
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-03-22 15:21:13 +0530

    Adding file ./01.js

---
+++ java_script_learning/day_01/01.js
@@ -0,0 +1,35 @@
+"use strict";       //  treate all JS ccode as a newer version
+
+
+// alert(2+3);      //  this is not browser
+
+
+// console.log("a");  console.log("b");        //  use of ";"
+
+
+
+// const a = 1 ;        // can't be redeclare Or reassigned 
+// let b = 2 ;      // can't be redeclare but can be reassigned
+// var c = 3 ;      // can be redeclare Or reassigned
+
+// console.table([a,b,c])
+
+
+// // Premitive datatypes :
+// number - 2^53
+// bigint - for large numbers
+// string - ""
+// boolean - true/false
+// null - standalone value
+// undefined - 
+// symbol - unique
+
+// // Non-Premitive datatypes :
+// object - 
+
+
+
+// // typeof - to get datatype
+// console.log(typeof null)        // object
+// console.log(typeof undefined)        // undefined
+// console.log(typeof true)        // boolean
