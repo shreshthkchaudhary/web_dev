bbd95715951df15047db82fec530a79e2e49520e
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-03-26 17:54:12 +0530

    Adding Stack & Heap Content

---
+++ java_script_learning/day_02/01.js
@@ -0,0 +1,13 @@
+// Stack (Primitive DataType) & Heap (Non-Primitive DataType)
+
+// HEAP Eg:
+// let user={
+//     name:"abc",
+//     number:123
+// }
+// console.log(user)
+// let user_trial=user
+// user_trial.name="xyz"
+// user_trial.number=987
+// console.log(user_trial)
+// console.log(user)       // change in user through refernce used by user_trial
