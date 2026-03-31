043e1dc0ca78e35aa064f674c1cda3faa2d7f372
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-03-31 23:51:34 +0530

    Adding Style

--- assignment/react_assignments/day_4/assignment_profile_card/src/components/Title_1.jsx
+++ assignment/react_assignments/day_4/assignment_profile_card/src/components/Title_1.jsx
@@ -1,6 +1,6 @@
 function Title1(){
     return (
-        <div style={{display:"flex",flexDirection:"column", margin:"0px"}}>
+        <div style={{display:"flex",flexDirection:"column", position:"absolute", top:"0px", left:"0px"}}>
             <h1>Shreshth Kumar Chaudhary</h1>
             <h2>Student</h2>
         </div>
