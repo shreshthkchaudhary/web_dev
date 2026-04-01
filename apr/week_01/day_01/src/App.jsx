485623ca16c74bf95286f858b9820a483ced6182
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-01 15:03:00 +0530

    Adding Redux Content

--- apr/week_01/day_01/src/App.jsx
+++ apr/week_01/day_01/src/App.jsx
@@ -5,15 +5,13 @@ function App() {
     <>
       <h1>App</h1>
       <Avt
-      name="shreshth"
-      id={2501010080}
-      data={["Game","Etah","Male"]}
-      link="https://webaim.org/techniques/images/media/hand_muscles.jpg"
-      status={true}
+        name={"shreshth"}
+        id={2501010080}
+        data={["Game", "Etah", "Male"]}
+        address={{ "a": "etah" }}
+        link={"https://webaim.org/techniques/images/media/hand_muscles.jpg"}
+        status={true}
       />
-
-      
-
     </>
   )
 }
--- apr/week_01/day_01/src/Components/Avt.jsx
+++ apr/week_01/day_01/src/Components/Avt.jsx
@@ -1,14 +1,20 @@
 function Avt(prop) {
+    const arr = [1,2,3,4,5];
+    const arr1 =["a","b","c","d"]
+    const ans = arr.map((el)=><li>{el}</li>)
+    const ans1 = arr1.map((el)=><li>{el}</li>)
     return (
         <>
             <h1>Avt</h1>
             <h2>Name:{prop.name}</h2>
             <h2>Roll No. :{prop.id}</h2>
             <h3>Hobby : {prop.data[0]}</h3>
-            <h3>Address : {prop.data[1]}</h3>
+            <h3>Address : {prop.address.a}</h3>
             <h3>Gender : {prop.data[2]}</h3>
             <h3><img src={prop.link} alt="" /></h3>
             <h1>Status : {prop.status ? "Completed":"Not Completed "}</h1>
+            <ol>{ans}</ol>
+            <ul typeof="square">{ans1}</ul>
         </>
     )
 }
