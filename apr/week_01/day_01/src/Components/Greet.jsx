1a4c76b2edf827fd47cc18ca614f00b0c818efe6
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-04 23:37:39 +0530

    Modifying CSS

--- apr/week_01/day_01/src/Components/Greet.jsx
+++ apr/week_01/day_01/src/Components/Greet.jsx
@@ -2,7 +2,7 @@ import styles from "./Greet.module.css";
 function Greet(){
     return(
         <>
-        <h1 className={styles.App}>Greet</h1>
+        <h1 className={styles.App}>Greet</h1>;
         </>
     )
 }
