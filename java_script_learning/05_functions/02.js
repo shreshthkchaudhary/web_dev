ce840a9d55c294a46d269db0feee41539e0f62cf
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-17 18:21:41 +0530

    Adding Content

---
+++ java_script_learning/05_functions/02.js
@@ -0,0 +1,37 @@
+// function nums(n){
+//     return n
+// }
+// console.log(nums(1,2,3,4))
+// // 1
+
+
+
+// // Rest operator in function:
+
+// function nums1(...n){   //  Returns an array
+//     return n
+// }
+// console.log(nums1(1,2,3,4))
+// // [ 1, 2, 3, 4 ]
+
+// function nums2(n1,n2,...n){
+//     return n
+// }
+// console.log(nums2(1,2,3,4))
+// //  [ 3, 4 ]
+
+
+
+// // Object in function:
+// let o1 = {
+//     name:"shreshth",
+//     id:12345
+// }
+// function f1(o){
+//     return `hi i am ${o.name} and my id is ${o.id}`
+// }
+// console.log(f1(o1))
+// console.log(f1({
+//     name:"ishu",
+//     id:9876
+// }))
