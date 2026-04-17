d99e23858943d899b0c84198edf2ff779b089d66
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-17 14:21:01 +0530

    Adding Content

--- java_script_learning/day_03/02.js
+++ java_script_learning/day_03/02.js
@@ -22,3 +22,30 @@ let l2=["a","b","c","d","e","f"]
 // //   6,   'a', 'b', 'c', 'd',
 // //   'e', 'f'
 // // ]
+
+// let l3=[1,2,3,[4,5,6,7,[8,9]],[10,11]]
+// l3=l3.flat(Infinity)
+// console.log(l3)
+// // [
+// //    1, 2, 3, 4,  5,
+// //    6, 7, 8, 9, 10,
+// //   11
+// // ]
+
+
+// console.log(Array.isArray("Hitesh"))
+// // false
+
+
+// console.log(Array.from("Hitesh"))
+// // [ 'H', 'i', 't', 'e', 's', 'h' ]
+
+// console.log(Array.from({n:"trail"}))
+// // []
+
+
+// let a=10
+// let b=20
+// let c=30
+// console.log(Array.of(a,b,c))
+// // [ 10, 20, 30 ]
