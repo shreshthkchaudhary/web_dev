27e319d19148984a3e94ffe53f44b80e49cafca7
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-19 18:14:45 +0530

    Adding Content

---
+++ java_script_learning/07_iterations/01_for.js
@@ -0,0 +1,8 @@
+// for:
+
+let array = [1,2,3,4]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+    
+}
---
+++ java_script_learning/07_iterations/02_break_continue.js
@@ -0,0 +1,32 @@
+// break:
+
+for (let a=1; a<=10; a++){
+    if (a==5){
+        break
+    }
+    console.log(a);
+}
+// 1
+// 2
+// 3
+// 4
+
+
+
+// continue:
+
+for (let a=1; a<=10; a++){
+    if (a==5){
+        continue
+    }
+    console.log(a);
+}
+// 1
+// 2
+// 3
+// 4
+// 6
+// 7
+// 8
+// 9
+// 10
