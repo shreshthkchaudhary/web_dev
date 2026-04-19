e5b3a2027bf9c3088eebb5e706d0ca736e7026c5
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-19 18:29:16 +0530

    Adding Content

---
+++ java_script_learning/07_iterations/03_while_dowhile.js
@@ -0,0 +1,53 @@
+// while:
+
+// let a=0
+// while (a<=8) {
+//     console.log(a)
+//     a++
+// }
+// // 0
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+// // 6
+// // 7
+// // 8
+
+// let l1=["a","b","c","d","e"]
+// let index=0
+// while(index<l1.length){
+//     console.log(l1[index])
+//     index++
+// }
+// // a
+// // b
+// // c
+// // d
+// // e
+
+
+
+
+// do while: Do part run first Then condition check in while
+
+// let s=1
+// do {
+//     console.log(s)
+//     s=s+6
+// } while (s<40);
+// // 1
+// // 7
+// // 13
+// // 19
+// // 25
+// // 31
+// // 37
+
+// let s=1
+// do {
+//     console.log(s)
+//     s=s+6
+// } while (s>40);
+// // 1
