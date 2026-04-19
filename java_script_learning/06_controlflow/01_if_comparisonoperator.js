34f15aee892297eb1fbffe588d1312789a196b09
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-19 17:12:51 +0530

    Adding Content

---
+++ java_script_learning/06_controlflow/01_if_comparisonoperator.js
@@ -0,0 +1,57 @@
+// // if
+
+// const userloggedin= true
+// if (userloggedin) {
+//     console.log("yes");
+// }
+
+
+// // if-else
+// if (userloggedin) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+
+
+// // if-else if-else
+// let a=500
+// if (a>1000) {
+//     console.log("a<1000");
+// } else if (a<=500) {
+//     console.log("a<500");
+// } else {
+//     console.log("no");
+// }
+
+
+// let a = true
+// let b = true
+// let c = false
+// let d = false
+// // &&   // Condition is check all are true
+// if (a&&b) {
+//     console.log("yes &&");
+// }
+// // yes &&
+
+// if (a&&b&&c) {
+//     console.log("yes &&");
+// }
+
+
+// // ||   //  Condition is check at least any one is true
+// if (a||b) {
+//     console.log("yes ||");
+// }
+// // yes ||
+
+// if (a||b||c) {
+//     console.log("yes ||");
+// }
+// // yes ||
+
+// if (c||d) {
+//     console.log("yes ||");
+// }
