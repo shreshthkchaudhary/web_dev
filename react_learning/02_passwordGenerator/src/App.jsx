51a361f786f44555f8c643c6990d0f9fcb268b40
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-04-28 20:27:57 +0530

    adding content

---
+++ react_learning/02_passwordGenerator/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()_+"
+  
+  },[length,numberAllowed,charAllowed,setPassword])
+  return (
+    <div className='w-full h-screen bg-black'>
+      <h1 className='text-white text-4xl text-center'>Password Generator</h1>
+
+    </div>
+  )
+}
+
+export default App
