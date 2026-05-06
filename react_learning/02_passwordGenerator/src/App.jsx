e947415d144ffe3e7c9d5c810c4249528ba1f447
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-05-07 01:42:33 +0530

    adding content

--- react_learning/02_passwordGenerator/src/App.jsx
+++ react_learning/02_passwordGenerator/src/App.jsx
@@ -6,9 +6,6 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
-
-
-
   const passwordGenerator = useCallback(()=>{
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
